ae91d590ca5fbf83e77d1b42dede466780800854
S <s>
2013-02-20 17:55:30 -0800

    {"type":"TI", "runs":"1"}

--- Breakout.java
+++ Breakout.java
@@ -81,7 +81,7 @@ public class Breakout extends GraphicsProgram {
 		GRect currentBrick;
 		for(int i = 0; i <= 9; i++) {
 			for(int j = 0; j <= 9; j++) {
-				currentBrick = new GRect()
+				currentBrick = new GRect();
 			}
 		}
 	}
