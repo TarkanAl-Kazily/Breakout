d958df623c7842f95cb2f20bd935d67c183e2c7a
S <s>
2013-02-23 21:01:04 -0800

    {"type":"TI", "runs":"1"}

--- Breakout.java
+++ Breakout.java
@@ -267,7 +267,7 @@ public class Breakout extends GraphicsProgram {
 	int ballYVelocity;
 	
 	/** Counts how many bricks have been destroyed. */
-	int counter = 100;
+	int counter = NBRICKS_PER_ROW * 10;
 	
 	/** Keeps track of how many turns the player has left */
 	int currentTurns = NTURNS;
