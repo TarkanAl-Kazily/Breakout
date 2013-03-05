0fcb29ee295691d0b5145f7ac6265a6d56d884d3
S <s>
2013-03-05 12:50:21 -0800

    {"type":"TI", "runs":"2"}

--- Breakout.java
+++ Breakout.java
@@ -157,7 +157,7 @@ public class Breakout extends GraphicsProgram {
 				ballXVelocity *= -1;
 			}
 			checkForCollisionsX(ball, BALL_RADIUS);
-			if (ball.getY() <= 0 || (ball.getY() + (2 * BALL_RADIUS) <= height)) {
+			if (ball.getY() <= 0 || (ball.getY() + (2 * BALL_RADIUS) <= HEIGHT)) {
 				ballYVelocity *= -1;	
 				}
 			if (checkForCollisionsY(ball, BALL_RADIUS)) {
